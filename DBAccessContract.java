4dfd23b9a44eba2ef8bc2be20736b66cdb1eefb1
Bruno Thalmann <mr.thalmann@gmail.com>
2015-03-12 11:26:26 +0100

    added packagename as static string

--- DBAccessContract.java
+++ DBAccessContract.java
@@ -2,4 +2,6 @@ package dk.aau.cs.psylog.module_lib;
 
 public final class DBAccessContract {
     public static final String DBACCESS_CONTENTPROVIDER = "content://dk.aau.cs.psylog.psylog/";
+    // Packagename: dk.aau.cs.psylog. '.' is replaced by '_' for avoiding collision with SQLite syntax
+    public static final String PACKAGE_NAME = "dk_aau_cs_psylog_";
 }
