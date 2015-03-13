8f7198c02c2037f189833de35faf17117b9a5932
Bruno Thalmann <mr.thalmann@gmail.com>
2015-03-13 11:04:04 +0100

    updated uri for the contentresolver after moving dal to its own namespace

--- DBAccessContract.java
+++ DBAccessContract.java
@@ -1,6 +1,6 @@
 package dk.aau.cs.psylog.module_lib;
 
 public final class DBAccessContract {
-    public static final String DBACCESS_CONTENTPROVIDER = "content://dk.aau.cs.psylog.psylog/";
+    public static final String DBACCESS_CONTENTPROVIDER = "content://dk.aau.cs.psylog.data_access_layer/";
 
 }
